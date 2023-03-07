188ae46ede9ee74404925e94f39bea3e2af0e320
Bobzero <rajabu.mustafa.00@gmail.com>
2023-03-07 20:13:29 +0300

    SUCCESSFULLY CONNECTED TO DATABASE

---
+++ BookStore/src/main/java/com/BookStore/BookStore/model/Book.java
@@ -0,0 +1,24 @@
+package com.BookStore.BookStore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+}
