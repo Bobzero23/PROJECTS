e86a0d399c9d5c3cac96d66785045c1711451e1c
Bobzero <rajabu.mustafa.00@gmail.com>
2023-06-07 14:32:28 +0300

    getting started with GuestBook project

---
+++ GuestBook/src/main/java/com/GuestBook/GuestBookApplication.java
@@ -0,0 +1,13 @@
+package com.GuestBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBookApplication.class, args);
+	}
+
+}
---
+++ GuestBook/src/test/java/com/GuestBook/GuestBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GuestBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
