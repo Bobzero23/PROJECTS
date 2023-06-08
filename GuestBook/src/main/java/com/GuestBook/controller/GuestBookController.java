c9bbae8142e46116205e135b1e890bbe70391709
Bobzero <rajabu.mustafa.00@gmail.com>
2023-06-08 12:25:03 +0300

    validations implemented successfully

--- GuestBook/src/main/java/com/GuestBook/controller/GuestBookController.java
+++ GuestBook/src/main/java/com/GuestBook/controller/GuestBookController.java
@@ -2,12 +2,16 @@ package com.GuestBook.controller;
 
 import com.GuestBook.model.GuestBook;
 import com.GuestBook.service.GuestBookService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class GuestBookController {
 
@@ -19,13 +23,17 @@ public class GuestBookController {
 
     @RequestMapping(value = {"", "/", "/home", "/form"})
     public String displayIndexPage(Model model) {
-        model.addAttribute("bookGuest", new GuestBook());
+        model.addAttribute("guestBook", new GuestBook());
         return "guestbook.html";
     }
 
     @PostMapping("/submitForm")
-    public String submitForm(@ModelAttribute GuestBook bookGuest) {
-        bookGuestService.saveGuestBook(bookGuest);
+    public String submitForm(@Valid @ModelAttribute GuestBook guestBook, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("failed to submit the form due to: " +  errors.toString());
+            return "guestbook.html";
+        }
+        bookGuestService.saveGuestBook(guestBook);
         return "redirect:/form";
     }
 }
--- GuestBook/src/main/java/com/GuestBook/model/GuestBook.java
+++ GuestBook/src/main/java/com/GuestBook/model/GuestBook.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -12,10 +13,26 @@ public class GuestBook {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "first name field can't be blank")
+    @Size(min = 3, message = "first name must be at least 3 characters")
     private String firstName;
+
+    @NotBlank(message = "last name name field can't be blank")
+    @Size(min = 3, message = "last name must be at least 3 characters")
     private String lastName;
+
+    @NotBlank(message = "email field can't be blank")
+    @Email
     private String email;
+
+    @NotBlank(message = "mobile number field can't be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "mobile number must have 10 digits")
     private String mobileNumber;
+
+    @NotBlank(message = "subject field can't be blank")
     private String subject;
+
+    @NotBlank(message = "message field can't be blank")
     private String message;
 }
