24103d7238db13d2a27f2d245c8582d4e2c7fada
Bobzeri23 <rajabu.mustafa.00@gmail.com>
2023-08-17 17:53:54 +0300

    basic authentication impletemation

---
+++ SpringSecurityWithAmigos/src/main/java/com/SpringSecurityWithAmigos/SpringSecurityWithAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityWithAmigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithAmigosApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityWithAmigos/src/main/java/com/SpringSecurityWithAmigos/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.SpringSecurityWithAmigos.config;public class SecurityConfig {
+}
