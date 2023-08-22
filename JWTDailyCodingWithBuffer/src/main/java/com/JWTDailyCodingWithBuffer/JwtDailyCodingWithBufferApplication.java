fc503f3500822ba9bc395cff27adb5f7cb06cd69
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-08-22 14:50:36 +0300

    jwt daily-coding-with-buffer

---
+++ JWTDailyCodingWithBuffer/src/main/java/com/JWTDailyCodingWithBuffer/JwtDailyCodingWithBufferApplication.java
@@ -0,0 +1,13 @@
+package com.JWTDailyCodingWithBuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDailyCodingWithBufferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDailyCodingWithBufferApplication.class, args);
+	}
+
+}
