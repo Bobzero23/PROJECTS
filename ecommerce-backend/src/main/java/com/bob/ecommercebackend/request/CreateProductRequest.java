2efbdc75936f6bf0b6e5d093d8a1c509db3274b3
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-11-22 13:39:10 +0300

    working on other models

---
+++ ecommerce-backend/src/main/java/com/bob/ecommercebackend/request/CreateProductRequest.java
@@ -0,0 +1,127 @@
+package com.bob.ecommercebackend.request;
+
+import com.bob.ecommercebackend.model.Size;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CreateProductRequest {
+
+    private String title;
+    private String description;
+    private int price;
+    private int discountedPrice;
+    private int discountPercent;
+    private int quantity;
+    private String brand;
+    private String color;
+    private Set<Size> sizes = new HashSet<>();
+    private String imageUrl;
+    private String topLavelCategory;
+    private String secondLavelCategory;
+    private String thirdLavelCategory;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public void setDiscountedPrice(int discountedPrice) {
+        this.discountedPrice = discountedPrice;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Set<Size> getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(Set<Size> sizes) {
+        this.sizes = sizes;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTopLavelCategory() {
+        return topLavelCategory;
+    }
+
+    public void setTopLavelCategory(String topLavelCategory) {
+        this.topLavelCategory = topLavelCategory;
+    }
+
+    public String getSecondLavelCategory() {
+        return secondLavelCategory;
+    }
+
+    public void setSecondLavelCategory(String secondLavelCategory) {
+        this.secondLavelCategory = secondLavelCategory;
+    }
+
+    public String getThirdLavelCategory() {
+        return thirdLavelCategory;
+    }
+
+    public void setThirdLavelCategory(String thirdLavelCategory) {
+        this.thirdLavelCategory = thirdLavelCategory;
+    }
+}
---
+++ ecommerce-backend/src/main/java/com/bob/ecommercebackend/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.bob.ecommercebackend.service;
+
+import com.bob.ecommercebackend.exception.ProductException;
+import com.bob.ecommercebackend.model.Product;
+import com.bob.ecommercebackend.request.CreateProductRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ProductService {
+    public Product createProduct(CreateProductRequest request);
+    public String deleteProduct(Long productId) throws ProductException;
+    public Product updateProduct(Long productId) throws ProductException;
+    public Product findProductById(Long productId) throws ProductException;
+    public List<Product> findProductByCategory(String category);
+    public Page<Product> getAllProducts();
+}
