833340bbc2f6edca248e9e834baab37d80dbdfa9
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-07 22:31:37 +0300

    task user service init

---
+++ Task-Management-Microservice/task-user-service/src/test/java/com/bob/TaskUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
