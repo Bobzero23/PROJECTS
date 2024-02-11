689fd157872f91d138be878a4902142c63e25a01
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-11 13:21:48 +0300

    jwt validator class implementation complete

---
+++ Task-Management-Microservice/task-user-service/src/main/java/com/bob/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.bob.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "sdluwoppejgalsdjtewjgaodkfaejdjgalheowajtaietpwjagoitewp";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ Task-Management-Microservice/task-user-service/src/main/java/com/bob/config/JwtProvider.java
@@ -0,0 +1,4 @@
+package com.bob.config;
+
+public class JwtProvider {
+}
